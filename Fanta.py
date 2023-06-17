c47cf6e4e17c6ef55cdea8e40c93b080230e7fef
Elliott Suh <elliott.w.suh@gmail.com>
2023-06-16 20:55:50 -0700

    added buttons

--- Fanta.py
+++ Fanta.py
@@ -14,6 +14,8 @@ BLACK = (0, 0, 0)
 
 BOARD_IMAGE = pygame.image.load("chess board.png")
 MENU_IMAGE = pygame.image.load("menu.png")
+ONE_PLAYER_IMAGE = pygame.image.load("1 player button.png")
+TWO_PLAYER_IMAGE = pygame.image.load("2 player button.png")
 
 #--------------------------------------------------------------------
 
@@ -21,11 +23,13 @@ game_scene = scene_class.Game_Scene(WIDTH, HEIGHT, BOARD_IMAGE)
 menu_scene = scene_class.Menu_Scene(WIDTH, HEIGHT, MENU_IMAGE)
 
     # On the menu screen
-#one_player_button = button_class.button(menu)
+one_player_button = button_class.Button(0, 0, ONE_PLAYER_IMAGE)
+#one_player_button_pos = functions.position(menu_scene, one_player_button, 1/2, 1/5)
+#one_player_button.change_position(one_player_button_pos)
 
 #--------------------------------------------------------------------
 
-
+menu_scene.add("1", one_player_button)
 
 #--------------------------------------------------------------------
 
@@ -41,19 +45,20 @@ def game_loop():
     functions.play_sounds()
 
 def menu_loop():
-    while menu_scene.active:
-        functions.check_user_input(menu_scene)
-        functions.resolve_collisions()
-        functions.draw_scene(menu_scene, window)
-        functions.play_sounds()
-        c.tick(FPS)
+    functions.check_user_input(menu_scene)
+    functions.resolve_collisions()
+    functions.draw_scene(menu_scene, window)
+    functions.play_sounds()
+        
 
 def program_loop():
     while menu_scene.active or game_scene.active:
         if menu_scene.active:
             menu_loop()
+            c.tick(FPS)
         elif game_scene.active:
             game_loop()
+            c.tick(FPS)
 
 #------------------------------------------------------------------
 program_loop()
--- functions.py
+++ functions.py
@@ -1,21 +1,97 @@
 import pygame
 
+def position(surface, object, x_proportion, y_proportion):
+    """
+    Returns top x and y based on the proportions given and the width/height and the width and height of the object
+
+    Parameters:
+    surface (class): width and height will be used for the new position
+    object (class or rect): width and height will be used for the new position
+    x_proportions (float): a fraction between 0 and 1
+    y_proportions (float): a fraction between 0 and 1
+
+    Returns:
+    new_x (int): the top x of the new position
+    new_y (int): the top y of the new position
+    """
+    new_center_x = surface.width * x_proportion
+    new_center_y = surface.height * y_proportion
+    
+    new_x = new_center_x - (object.width / 2)
+    new_y = new_center_y - (object.height / 2)
+    return (new_x, new_y)
+
 def check_user_input(scene):
+    """
+    Checks for user input in the scene
+
+    Parameters:
+    scene (class): the surface that checks for user input
+
+    Returns
+    None
+    """
     scene.handle_keyboard()
 
 def run_ai():
+    """
+    Runs the ai of the computer
+
+    Parameters:
+    None
+
+    Returns
+    None
+    """
     pass
 
 def move_everything():
+    """
+    Moves everything in the scene
+
+    Parameters:
+    None
+
+    Returns
+    None
+    """
     pass
 
 def resolve_collisions():
+    """
+    Checks for colliding objects in the scene
+
+    Parameters:
+    None
+
+    Returns
+    None
+    """
     pass
 
 def play_sounds():
+    """
+    Plays sounds in the scene based on what happened
+
+    Parameters:
+    None
+
+    Returns
+    None
+    """
     pass
 
 def draw_scene(scene, window):
+    """
+    Draws a scene surface onto the main window
+
+    Parameters:
+    scene (class): the surface that will be drawn
+    window (window): the surface that the scene is being drawn on
+
+    Returns
+    None
+    """
     scene.draw()
     window.blit(scene.surface, (0, 0))
     pygame.display.flip()
